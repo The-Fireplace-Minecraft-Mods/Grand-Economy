25cf7fcc3d7b1652a2af28ebeb0cf32d81c54fba
The_Fireplace <the.f1repl4ce@gmail.com>
2019-02-15 23:27:10 -0600

    Fix commands

--- src/main/java/the_fireplace/grandeconomy/GeCommands.java
+++ src/main/java/the_fireplace/grandeconomy/GeCommands.java
@@ -47,49 +47,49 @@ public class GeCommands {
 
         LiteralArgumentBuilder<CommandSource> walletCommand = Commands.literal("wallet").requires((iCommandSender) -> iCommandSender.getEntity() instanceof EntityPlayer && iCommandSender.hasPermissionLevel(2));
 
-        walletCommand.then(Commands.literal("balance")).executes((command) -> {
+        walletCommand.then(Commands.literal("balance").executes((command) -> {
             Account account = Account.get(command.getSource().asPlayer());
             account.update();
             long balance = account.getBalance();
             command.getSource().sendFeedback(new TextComponentTranslation("Balance: %s", balance), false);
             return 1;
-        }).then(Commands.argument("target", EntityArgument.singlePlayer())).executes((command) -> {
+        }).then(Commands.argument("target", EntityArgument.singlePlayer()).executes((command) -> {
             EntityPlayer target = EntityArgument.getOnePlayer(command, "target");
             Account pAccount = Account.get(target);
             pAccount.update();
             command.getSource().sendFeedback(new TextComponentTranslation("%s balance: %s", target.getName(), pAccount.getBalance()), false);
             return 1;
-        });
-        walletCommand.then(Commands.literal("set"))
-                .then(Commands.argument("target", EntityArgument.singlePlayer()))
-                .then(Commands.argument("amount", IntegerArgumentType.integer(0)))
+        })));
+        walletCommand.then(Commands.literal("set")
+                .then(Commands.argument("target", EntityArgument.singlePlayer())
+                .then(Commands.argument("amount", IntegerArgumentType.integer(0))
                 .executes((command) -> {
             EntityPlayer target = EntityArgument.getOnePlayer(command, "target");
             Account pAccount = Account.get(target);
             pAccount.setBalance(IntegerArgumentType.getInteger(command, "amount"), false);
             command.getSource().sendFeedback(new TextComponentTranslation("%s balance set to %s", target.getName(), pAccount.getBalance()), false);
             return 1;
-        });
-        walletCommand.then(Commands.literal("give"))
-                .then(Commands.argument("target", EntityArgument.singlePlayer()))
-                .then(Commands.argument("amount", IntegerArgumentType.integer(0)))
+        }))));
+        walletCommand.then(Commands.literal("give")
+                .then(Commands.argument("target", EntityArgument.singlePlayer())
+                .then(Commands.argument("amount", IntegerArgumentType.integer(0))
                 .executes((command) -> {
                     EntityPlayer target = EntityArgument.getOnePlayer(command, "target");
                     Account pAccount = Account.get(target);
                     pAccount.addBalance(IntegerArgumentType.getInteger(command, "amount"), false);
                     command.getSource().sendFeedback(new TextComponentTranslation("%s balance is now %s", target.getName(), pAccount.getBalance()), false);
                     return 1;
-                });
-        walletCommand.then(Commands.literal("take"))
-                .then(Commands.argument("target", EntityArgument.singlePlayer()))
-                .then(Commands.argument("amount", IntegerArgumentType.integer(0)))
+                }))));
+        walletCommand.then(Commands.literal("take")
+                .then(Commands.argument("target", EntityArgument.singlePlayer())
+                .then(Commands.argument("amount", IntegerArgumentType.integer(0))
                 .executes((command) -> {
                     EntityPlayer target = EntityArgument.getOnePlayer(command, "target");
                     Account pAccount = Account.get(target);
                     pAccount.addBalance(-IntegerArgumentType.getInteger(command, "amount"), false);
                     command.getSource().sendFeedback(new TextComponentTranslation("%s balance is now %s", target.getName(), pAccount.getBalance()), false);
                     return 1;
-                });
+                }))));
 
         commandDispatcher.register(walletCommand);
     }
