4b35dc425ec3f2f0a66d01180d2e869d7ac5916c
The_Fireplace <the.f1repl4ce@gmail.com>
2020-02-11 07:43:05 -0600

    Fixed compile errors

--- fabric/src/main/java/the_fireplace/grandeconomy/fabric/events/KillingEvents.java
+++ fabric/src/main/java/the_fireplace/grandeconomy/fabric/events/KillingEvents.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import the_fireplace.grandeconomy.api.GrandEconomyApi;
 import the_fireplace.grandeconomy.fabric.Config;
+import the_fireplace.grandeconomy.fabric.translation.TranslationUtil;
 
 public class KillingEvents {
     public static void onPlayerDeath(ServerPlayerEntity entity, DamageSource source) {
@@ -22,10 +23,10 @@ public class KillingEvents {
                 Math.max(Math.min(GrandEconomyApi.getBalance(entity.getUuid(), true), -Config.pvpMoneyTransfer), 0);
             GrandEconomyApi.takeFromBalance(entity.getUuid(), amountTaken, true);
 
-            entity.sendMessage(TranslationUtil.getTranslation(entity.getUniqueID(), "grandeconomy.killed_balance", GrandEconomyApi.getBalance(entity.getUuid(), true)));
+            entity.sendMessage(TranslationUtil.getTranslation(entity.getUuid(), "grandeconomy.killed_balance", GrandEconomyApi.getBalance(entity.getUuid(), true)));
 
             GrandEconomyApi.addToBalance(killer.getUuid(), amountTaken, true);
-            killer.sendMessage(TranslationUtil.getTranslation(killer.getUniqueID(), "grandeconomy.killer_balance", GrandEconomyApi.getBalance(killer.getUuid(), true)));
+            killer.sendMessage(TranslationUtil.getTranslation(killer.getUuid(), "grandeconomy.killer_balance", GrandEconomyApi.getBalance(killer.getUuid(), true)));
         }
     }
 }
