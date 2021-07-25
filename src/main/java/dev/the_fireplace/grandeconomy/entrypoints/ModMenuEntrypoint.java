e64e4da0c9e53eb7620b13b0b3ddbc05f8381766
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-25 14:15:41 -0500

    Backport changes to 1.14.4

--- src/main/java/dev/the_fireplace/grandeconomy/entrypoints/ModMenuEntrypoint.java
+++ src/main/java/dev/the_fireplace/grandeconomy/entrypoints/ModMenuEntrypoint.java
@@ -1,19 +1,26 @@
 package dev.the_fireplace.grandeconomy.entrypoints;
 
 import dev.the_fireplace.annotateddi.api.DIContainer;
+import dev.the_fireplace.grandeconomy.GrandEconomyConstants;
 import dev.the_fireplace.grandeconomy.config.GEConfigScreenFactory;
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
+
+import java.util.function.Function;
 
 @Environment(EnvType.CLIENT)
 public final class ModMenuEntrypoint implements ModMenuApi {
     private final GEConfigScreenFactory configScreenFactory = DIContainer.get().getInstance(GEConfigScreenFactory.class);
 
     @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (ConfigScreenFactory<Screen>) configScreenFactory::getConfigScreen;
+    public String getModId() {
+        return GrandEconomyConstants.MODID;
+    }
+
+    @Override
+    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+        return configScreenFactory::getConfigScreen;
     }
 }
