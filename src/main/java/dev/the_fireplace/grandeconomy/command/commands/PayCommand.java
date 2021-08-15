0f918b53a1d8ea87ce354c20bd21fac44b265f82
The_Fireplace <the.f1repl4ce@gmail.com>
2021-08-14 20:35:53 -0500

    Add ability to /pay offline players

--- src/main/java/dev/the_fireplace/grandeconomy/command/commands/PayCommand.java
+++ src/main/java/dev/the_fireplace/grandeconomy/command/commands/PayCommand.java
@@ -10,10 +10,11 @@ import dev.the_fireplace.grandeconomy.api.injectables.CurrencyAPI;
 import dev.the_fireplace.grandeconomy.command.CommonTranslationKeys;
 import dev.the_fireplace.grandeconomy.command.GeCommand;
 import dev.the_fireplace.lib.api.chat.injectables.TranslatorFactory;
+import dev.the_fireplace.lib.api.command.injectables.ArgumentTypeFactory;
 import dev.the_fireplace.lib.api.command.injectables.FeedbackSenderFactory;
 import dev.the_fireplace.lib.api.command.injectables.Requirements;
+import dev.the_fireplace.lib.api.command.interfaces.PossiblyOfflinePlayer;
 import net.minecraft.command.CommandException;
-import net.minecraft.command.arguments.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -25,16 +26,19 @@ import java.util.UUID;
 @Singleton
 public final class PayCommand extends GeCommand {
 
+    private final ArgumentTypeFactory argumentTypeFactory;
+
     @Inject
-    public PayCommand(CurrencyAPI currencyAPI, TranslatorFactory translatorFactory, FeedbackSenderFactory feedbackSenderFactory, Requirements requirements) {
+    public PayCommand(CurrencyAPI currencyAPI, TranslatorFactory translatorFactory, FeedbackSenderFactory feedbackSenderFactory, Requirements requirements, ArgumentTypeFactory argumentTypeFactory) {
         super(currencyAPI, translatorFactory, feedbackSenderFactory, requirements);
+        this.argumentTypeFactory = argumentTypeFactory;
     }
 
     @Override
     public CommandNode<ServerCommandSource> register(CommandDispatcher<ServerCommandSource> commandDispatcher) {
         return commandDispatcher.register(CommandManager.literal("pay")
             .requires(requirements::player)
-            .then(CommandManager.argument("player", EntityArgumentType.player())
+            .then(CommandManager.argument("player", argumentTypeFactory.possiblyOfflinePlayer())
                 .then(CommandManager.argument("amount", DoubleArgumentType.doubleArg(0))
                     .executes(this::execute)
                 )
@@ -43,7 +47,7 @@ public final class PayCommand extends GeCommand {
     }
 
     private int execute(CommandContext<ServerCommandSource> command) throws CommandException, CommandSyntaxException {
-        ServerPlayerEntity targetPlayer = EntityArgumentType.getPlayer(command, "player");
+        PossiblyOfflinePlayer targetPlayer = argumentTypeFactory.getPossiblyOfflinePlayer(command, "player");
         double amount = command.getArgument("amount", Double.class);
         if (amount < 0) {
             return feedbackSender.throwFailure(command, "commands.grandeconomy.pay.negative");
@@ -55,10 +59,13 @@ public final class PayCommand extends GeCommand {
 
         boolean taken = currencyAPI.takeFromBalance(senderAccountId, amount, true);
         if (taken) {
-            UUID targetAccountId = targetPlayer.getUuid();
+            UUID targetAccountId = targetPlayer.getId();
             currencyAPI.addToBalance(targetAccountId, amount, true);
-            feedbackSender.basic(command, "commands.grandeconomy.pay.paid", currencyAPI.formatCurrency(amount), targetPlayer.getDisplayName());
-            feedbackSender.basic(targetPlayer, "commands.grandeconomy.pay.recieved", currencyAPI.formatCurrency(amount), command.getSource().getName());
+            feedbackSender.basic(command, "commands.grandeconomy.pay.paid", currencyAPI.formatCurrency(amount), targetPlayer.getName());
+            ServerPlayerEntity targetPlayerEntity = targetPlayer.entity();
+            if (targetPlayerEntity != null) {
+                feedbackSender.basic(targetPlayerEntity, "commands.grandeconomy.pay.recieved", currencyAPI.formatCurrency(amount), command.getSource().getName());
+            }
 
             return Command.SINGLE_SUCCESS;
         } else {
