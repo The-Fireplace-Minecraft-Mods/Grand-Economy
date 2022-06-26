957cbcc2d74da73fc226fca385d5b8e2bff7b836
The_Fireplace <git@the-fireplace.dev>
2022-06-26 12:01:14 -0500

    Update Diamond Economy compat

--- fabric/src/main/java/dev/the_fireplace/grandeconomy/adapters/DiamondEconomyAdapter.java
+++ fabric/src/main/java/dev/the_fireplace/grandeconomy/adapters/DiamondEconomyAdapter.java
@@ -1,37 +1,14 @@
 package dev.the_fireplace.grandeconomy.adapters;
 
-import com.gmail.sneakdevs.diamondeconomy.DatabaseManager;
-import com.mojang.authlib.GameProfile;
+import com.gmail.sneakdevs.diamondeconomy.DiamondEconomy;
+import com.gmail.sneakdevs.diamondeconomy.sql.DatabaseManager;
 import dev.the_fireplace.grandeconomy.api.interfaces.EconomyAdapter;
-import dev.the_fireplace.lib.api.player.injectables.GameProfileFinder;
 
-import javax.inject.Inject;
-import java.util.Optional;
 import java.util.UUID;
 
 public final class DiamondEconomyAdapter implements EconomyAdapter
 {
-    private final GameProfileFinder gameProfileFinder;
     private DatabaseManager databaseManager;
-
-    @Inject
-    public DiamondEconomyAdapter(GameProfileFinder gameProfileFinder) {
-        this.gameProfileFinder = gameProfileFinder;
-    }
-
-    private String findName(UUID uuid, Boolean isPlayer) {
-        String nonPlayerName = "Grand Economy Non-Player " + uuid.toString();
-        if (Boolean.FALSE.equals(isPlayer)) {
-            return nonPlayerName;
-        }
-
-        Optional<GameProfile> playerProfile = gameProfileFinder.findProfile(uuid);
-        if (playerProfile.isEmpty()) {
-            return nonPlayerName;
-        }
-
-        return playerProfile.get().getName();
-    }
 
     @Override
     public double getBalance(UUID uuid, Boolean isPlayer) {
@@ -42,7 +19,7 @@ public final class DiamondEconomyAdapter implements EconomyAdapter
     public boolean addToBalance(UUID uuid, double amount, Boolean isPlayer) {
         int previousBalance = databaseManager.getBalanceFromUUID(uuid.toString());
         int newBalance = previousBalance + (int) amount;
-        databaseManager.setBalance(uuid.toString(), findName(uuid, isPlayer), newBalance);
+        databaseManager.setBalance(uuid.toString(), newBalance);
         return true;
     }
 
@@ -50,13 +27,13 @@ public final class DiamondEconomyAdapter implements EconomyAdapter
     public boolean takeFromBalance(UUID uuid, double amount, Boolean isPlayer) {
         int previousBalance = databaseManager.getBalanceFromUUID(uuid.toString());
         int newBalance = Math.max(previousBalance - (int) amount, 0);
-        databaseManager.setBalance(uuid.toString(), findName(uuid, isPlayer), newBalance);
+        databaseManager.setBalance(uuid.toString(), newBalance);
         return true;
     }
 
     @Override
     public boolean setBalance(UUID uuid, double amount, Boolean isPlayer) {
-        databaseManager.setBalance(uuid.toString(), findName(uuid, isPlayer), (int) amount);
+        databaseManager.setBalance(uuid.toString(), (int) amount);
         return true;
     }
 
@@ -77,6 +54,6 @@ public final class DiamondEconomyAdapter implements EconomyAdapter
 
     @Override
     public void init() {
-        this.databaseManager = new DatabaseManager();
+        this.databaseManager = DiamondEconomy.getDatabaseManager();
     }
 }
